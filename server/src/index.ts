6ff93eef7c08ef7da400fc53dedf93dae2762df0
Rahul R Nair <nairrahul160@gmail.com>
2026-09-17 17:20:23 +0530

    wire server to serve built client, add root build/start scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 
@@ -6,6 +7,12 @@ app.use(cors());
 
 app.get("/api/health", (_req, res) => {
   res.json({ status: "ok" });
+});
+
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 3001;
